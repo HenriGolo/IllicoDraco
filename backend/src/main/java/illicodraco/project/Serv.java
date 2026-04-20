d29b7c5e885ca0e2d03a60d423e39ff76d17df08
Henri Golo <henri.golo.313f@gmail.com>
2026-04-20 14:18:00 +0200

    commentaires vieux code

--- backend/src/main/java/illicodraco/project/Serv.java
+++ backend/src/main/java/illicodraco/project/Serv.java
@@ -1,78 +1,78 @@
 package illicodraco.project;
 
-import java.io.IOException;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
 @WebServlet("/Serv")
 public class Serv extends HttpServlet {
- 
-    Facade f;
 
-    public Serv(){
-        f = new Facade();
-    }
+  Facade facade;
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-            String op = request.getParameter("op");
+  public Serv() {
+    facade = new Facade();
+  }
 
-            switch (op) {
-                case "ajoutp":
-                    String nom = request.getParameter("nom");
-                    String prenom = request.getParameter("prenom");
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    /*try {
+      String op = request.getParameter("op");
 
-                    f.ajoutPersonne(nom, prenom);
-                    request.getRequestDispatcher("index.html").forward(request, response);
-                    return;
+      switch (op) {
+        case "ajoutp":
+          String nom = request.getParameter("nom");
+          String prenom = request.getParameter("prenom");
 
-                case "ajouta":
-                    String rue = request.getParameter("rue");
-                    String ville = request.getParameter("ville");
+          facade.ajoutPersonne(nom, prenom);
+          request.getRequestDispatcher("index.html").forward(request, response);
+          return;
 
-                    f.ajoutAdresse(rue, ville);
-                    request.getRequestDispatcher("index.html").forward(request, response);
-                    return;
-                
-                case "choix":
-                    request.setAttribute("lp", f.listePersonnes());
-                    request.setAttribute("la", f.listeAdresses());
-                    
-                    request.getRequestDispatcher("choix.jsp").forward(request, response);
-                    return;
+        case "ajouta":
+          String rue = request.getParameter("rue");
+          String ville = request.getParameter("ville");
 
-                case "associer":
-                    int ip = Integer.parseInt(request.getParameter("idp"));
-                    int ia = Integer.parseInt(request.getParameter("ida"));
-                    f.associer(ip, ia);
-                    request.getRequestDispatcher("index.html").forward(request, response);
-                    return;
+          facade.ajoutAdresse(rue, ville);
+          request.getRequestDispatcher("index.html").forward(request, response);
+          return;
 
-                case "lister":
-                    request.setAttribute("lp", f.listePersonnes());
-                    request.setAttribute("la", f.listeAdresses());
-                
-                    request.getRequestDispatcher("lister.jsp").forward(request, response);
-                    return;
-                    
-                default:
-                    request.getRequestDispatcher("index.html").forward(request, response);
-                    return;
-            }
+        case "choix":
+          request.setAttribute("lp", facade.listePersonnes());
+          request.setAttribute("la", facade.listeAdresses());
+
+          request.getRequestDispatcher("choix.jsp").forward(request, response);
+          return;
+
+        case "associer":
+          int ip = Integer.parseInt(request.getParameter("idp"));
+          int ia = Integer.parseInt(request.getParameter("ida"));
+          facade.associer(ip, ia);
+          request.getRequestDispatcher("index.html").forward(request, response);
+          return;
+
+        case "lister":
+          request.setAttribute("lp", facade.listePersonnes());
+          request.setAttribute("la", facade.listeAdresses());
+
+          request.getRequestDispatcher("lister.jsp").forward(request, response);
+          return;
+
+        default:
+          request.getRequestDispatcher("index.html").forward(request, response);
+          return;
+      }
 
 
-        } catch (Exception e) {
-            response.getWriter().println("<html><body>ERREUR !</body></html>");
-        }
-    }
+    } catch (Exception e) {
+      response.getWriter().println("<html><body>ERREUR !</body></html>");
+    }// */
+  }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
+  }
 }
