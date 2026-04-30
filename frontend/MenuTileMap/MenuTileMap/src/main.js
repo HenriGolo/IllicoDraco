684a9053f590643422b8ede322489121ba1b491b
celinda42 <112560667+celinda42@users.noreply.github.com>
2026-04-30 16:02:10 +0200

    mis mon avancement sur ma branche
    input textuel pour le pseudo suivi de l'apparition du menu

---
+++ frontend/MenuTileMap/MenuTileMap/src/main.js
@@ -0,0 +1,34 @@
+import { Start } from './scenes/Start.js';
+import { Preloader } from './scenes/Preloader.js';
+
+const config = {
+    type: Phaser.AUTO,
+    title: 'Overlord Rising',
+    description: '',
+    parent: 'game-container',
+    width: 1280,
+    height: 720,
+    backgroundColor: '#000000',
+    pixelArt: true,
+    scene: [
+        Preloader,
+        Start
+    ],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: true
+        }
+    },
+    dom : {
+        createContainer : true
+    },
+}
+
+new Phaser.Game(config);
+            
