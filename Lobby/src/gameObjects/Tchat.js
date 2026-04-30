aa2c6f068373eaffc5fdacd4190b115e6a96a339
JulMig <julie.mignotte8@gmail.com>
2026-04-30 15:51:54 +0200

    Tchat ws

--- Lobby/src/gameObjects/Tchat.js
+++ Lobby/src/gameObjects/Tchat.js
@@ -1,8 +1,8 @@
 export default class Tchat {
 
-    constructor(scene, pseudo, x, y) {
+    constructor(scene, pseudo, x, y, serveur_url) {
    
-
+        this.serveur_url = serveur_url;
         this.pseudo = pseudo;
         //Affichage du tchat //1120 332
         this.tchatOutput = scene.add.dom(x + 160, y + 204).createFromCache('tchatTextOutput');
@@ -10,11 +10,19 @@ export default class Tchat {
         this.tchatInput = scene.add.dom(x + 160, y + 432).createFromCache('tchatTextInput');
 
         this.tchatInput.addListener('click');
-        this.tchatInput.on('click', (event) => this.send_text_to_tchat(event));
+        this.tchatInput.on('click', (event) => this.send_enter_text(event));
+
+        this.ws = new WebSocket(serveur_url);
         
+        this.ws.onopen = () => this.send_text(this.pseudo + " vient d'arriver dans la cuisine !");
+        this.ws.onmessage = (e) => this.add_text_to_tchat(e.content);
+        this.ws.onclose = () => this.send_text(this.pseudo + " vous a abandonné...");
+        this.ws.onerror = () => this.add_text_to_tchat(this.pseudo + " : Une erreur s'est produite");
+
     }
 
-    send_text_to_tchat(event) {
+    //récupère le texte entrée et l'envoie au tchat
+    send_enter_text(event) {
 
         //TODO Envoyer le texte au serveur
         if (event.target.name === 'sendToTchatButton')
@@ -22,11 +30,10 @@ export default class Tchat {
                 let inputText = this.tchatInput.getChildByName('textField');
                 if (inputText.value !== '')
                 {
-                    let text = document.getElementById("tchat_output").innerText;
-                    text = text + this.pseudo + " : " + inputText.value +  "\n";
+                    let text = this.pseudo + " : " + inputText.value;
                     inputText.value = "";
-                    //TODO Envoie du text
-
+                    //Envoie du text
+                    this.send_text(text);
                     //Ajout au tchat du joueur courant
                     //DEBUG
                     this.add_text_to_tchat(text)
@@ -34,12 +41,20 @@ export default class Tchat {
             }
     }
 
+    //Envoie le texte passé en paramètre au tchat via la websocket au serveur
+    send_text(text) {
+        if (text !== '') {
+            this.ws.send(text);
+        }
+    }
+
+    //Ajoute le texte sur le tchat
     add_text_to_tchat(text) {
      
-        //  Have they entered anything?
-        document.getElementById("tchat_output").innerText = text;
+        let textout = document.getElementById("tchat_output");
+        let newText = textout.innerText + text + "\n";
+        textout.innerText = newText;
                 
-            
     }
 
 }
