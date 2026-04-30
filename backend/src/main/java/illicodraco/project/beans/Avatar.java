78a3719d7fcea97a57701a5ab51a3cb1665a57d0
yjy2068 <yjy2068@troll.enseeiht.fr>
2026-04-30 16:34:31 +0200

    Modifs des java beans

---
+++ backend/src/main/java/illicodraco/project/beans/Avatar.java
@@ -0,0 +1,59 @@
+package illicodraco.project.beans;
+
+import jakarta.persistence.*;
+
+// Classe java pour gérer l'entité "avatar"
+// SANS ANNOTATIONS JPA CAR DYNAMIQUE ?
+public class Avatar {
+
+    // attributs
+
+    private int id;                 // clef primaire dans la BD
+
+    private Joueur joueur;          // joueur possédant l'avatar (joueur = owner)
+
+    private Classe classe;          // classe de l'avatar du joueur
+
+    private Produit produit;        // produit porté par le joueur
+
+    // constructeur
+    public Avatar() {
+    }
+
+
+    // getters
+    public int getId() {
+        return id;
+    }
+
+    public Joueur getJoueur() {
+        return joueur;
+    }
+
+    public Produit getProduit() {
+        return produit;
+    }
+
+    public Classe getClasse() {
+        return classe;
+    } 
+
+
+    // setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setJoueur(Joueur joueur) {
+        this.joueur = joueur;
+    }
+
+    public void setProduit(Produit produit) {
+        this.produit = produit;
+    }
+
+    public void setClasse(Classe classe) {
+        this.classe = classe;
+    }
+
+}
