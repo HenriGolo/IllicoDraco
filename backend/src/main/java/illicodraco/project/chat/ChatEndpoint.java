ae245b2227931fdbf6a4b45dcdad1e99fb9fd758
Henri Golo <henri.golo.313f@gmail.com>
2026-04-30 14:48:08 +0200

    mise en forme

--- backend/src/main/java/illicodraco/project/chat/ChatEndpoint.java
+++ backend/src/main/java/illicodraco/project/chat/ChatEndpoint.java
@@ -48,12 +48,10 @@ public class ChatEndpoint {
 
   @OnError
   public void onError(Session session, Throwable throwable) {
-    // Do error handling here
+    // TODO gestion d'erreurs
   }
 
-  private static void broadcast(Message message)
-    throws IOException, EncodeException {
-
+  private static void broadcast(Message message) throws IOException, EncodeException {
     chatEndpoints.forEach(endpoint -> {
       synchronized (endpoint) {
         try {
