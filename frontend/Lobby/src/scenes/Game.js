66c0e6efdeb049aea8553a103c83ea9696808c0d
JulMig <julie.mignotte8@gmail.com>
2026-05-01 17:09:53 +0200

    Barre d'informations

--- frontend/Lobby/src/scenes/Game.js
+++ frontend/Lobby/src/scenes/Game.js
@@ -13,6 +13,7 @@ export class Game extends Phaser.Scene {
         this.pseudo = pseudo;
 
         this.tchat;
+        this.infosText; //Affichage des informations
 
     }
 
@@ -32,23 +33,34 @@ export class Game extends Phaser.Scene {
 
     create() {
 
+        var graphics = this.add.graphics();
+
+        //Barre d'informations
+        graphics.lineStyle(4, 0xc1c1c1, 1.0);
+        graphics.fillStyle(0x6b4b34, 1);
+        graphics.fillRect(0, 0, 1280, 50);
+        graphics.strokeRect(2, 2, 1276, 48);    
+
+        this.infosText = this.add.text(0, 16, 'Temps : XX | Argent : XX', { fontSize: '24px', fill: '#ffffff' })
+        .setFixedSize(1280, 32)
+        .setAlign('center');
 
         //Bouton Boutique / Receuil
-        this.add.sprite(10 + (24*zoom)/2, 10 + (24*zoom)/2, 'bt_boutique').setScale(zoom, zoom)
+        this.add.sprite(10 + (24*zoom)/2, 60 + (24*zoom)/2, 'bt_boutique').setScale(zoom, zoom)
             .setInteractive()
             .on('pointerdown', () => this.open_boutique());
 
-        this.add.sprite(10 + (24*zoom)/2, 20 + (24*zoom)/2*3, 'bt_receuil').setScale(zoom, zoom)
+        this.add.sprite(10 + (24*zoom)/2, 70 + (24*zoom)/2*3, 'bt_receuil').setScale(zoom, zoom)
             .setInteractive()
             .on('pointerdown', () => this.open_receuil());
 
         //Bouton Parametre
-        this.add.sprite(1270 - (24*zoom)/2, 10 + (24*zoom)/2, 'bt_parametre').setScale(zoom, zoom)
+        this.add.sprite(1270 - (24*zoom)/2, 60 + (24*zoom)/2, 'bt_parametre').setScale(zoom, zoom)
             .setInteractive()
             .on('pointerdown', () => this.open_parameter());
 
         //Ajout du tchat
-        this.tchat = new Tchat(this, this.pseudo, 960, 128, this.serveur_url);
+        this.tchat = new Tchat(this, this.pseudo, 960, 178, this.serveur_url);
 
         //Initialiser les touches du jeu
         this.input.keyboard.on('keyup', (event) => this.handle_key(event))
