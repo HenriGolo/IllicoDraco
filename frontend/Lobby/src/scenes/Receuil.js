86a58acb9b90bb533ea471e7543160b17cd3a2fb
JulMig <julie.mignotte8@gmail.com>
2026-05-02 18:24:05 +0200

    Receuil affichage des recettes

--- frontend/Lobby/src/scenes/Receuil.js
+++ frontend/Lobby/src/scenes/Receuil.js
@@ -1,10 +1,24 @@
+import RecipePresentation from '../gameObjects/RecipePresentation.js';
+import Recipe from '../entities/Recipe.js';
+
     const zoom = 3;
     const nb_row_display = 2; //Nombre de ligne affiché par page 
     const nb_col_display = 4; //Nombre de col affiché par page
 
 export class Receuil extends Phaser.Scene {
 
-    constructor () {
+    constructor (recipes = [
+        new Recipe("Mage Vert", ['j3_guerrier', 'j3_archer', 'j3_pretre'], 'j3_mage', 'j1_mage'),
+        new Recipe("Mage Vert", ['j3_guerrier', 'j3_archer', 'j3_pretre'], 'j3_mage', 'j1_mage'),
+        new Recipe("Mage Vert", ['j3_guerrier', 'j3_archer', 'j3_pretre'], 'j3_mage', 'j1_mage'),
+        new Recipe("Mage Vert", ['j3_guerrier', 'j3_archer', 'j3_pretre'], 'j3_mage', 'j1_mage'),
+        new Recipe("Mage Vert", ['j3_guerrier', 'j3_archer', 'j3_pretre'], 'j3_mage', 'j1_mage'),
+        new Recipe("Mage Vert", ['j3_guerrier', 'j3_archer', 'j3_pretre'], 'j3_mage', 'j1_mage'),
+        new Recipe("Mage Vert", ['j3_guerrier', 'j3_archer', 'j3_pretre'], 'j3_mage', 'j1_mage'),
+        new Recipe("Mage Vert", ['j3_guerrier', 'j3_archer', 'j3_pretre'], 'j3_mage', 'j1_mage'),
+        new Recipe("Mage Vert", ['j3_guerrier', 'j3_archer', 'j3_pretre'], 'j3_mage', 'j1_mage'),
+        new Recipe("Mage Rouge", ['j2_guerrier', 'j2_archer', 'j2_pretre'], 'j2_mage', 'j4_mage')
+    ], monsters = []) {
         super('Receuil');
 
         this.recetteButton;
@@ -12,6 +26,12 @@ export class Receuil extends Phaser.Scene {
 
         this.total_monster; //Nombre de monstre
         this.total_recipe; //Nombre de recette
+
+        this.monstersPres;
+        this.recipesPres;
+
+        this.recipes = recipes;
+        this.monsters = monsters;
 
         this.curPageText;
 
@@ -63,9 +83,20 @@ export class Receuil extends Phaser.Scene {
             .on('pointerdown', () => this.nextPage());
 
         //Set les recettes
-        this.total_monster = 13; //TODO
+        this.total_monster = this.monsters.length;
         //Set les monstres
-        this.total_recipe = 32; //TODO
+        this.total_recipe = this.recipes.length;
+
+        this.recipesPres = [];
+        for (var y = 0; y < nb_row_display; y ++) {
+            for (var x = 0; x < nb_col_display; x++) {
+                this.recipesPres[x+(y*nb_col_display)] = new RecipePresentation(
+                    this,
+                    (315)*x+(5*(x+1))+5,
+                    (y*314) + 102
+                )
+            }
+        }
 
         this.switch_recette();
     }
@@ -75,20 +106,24 @@ export class Receuil extends Phaser.Scene {
     }
 
     switch_recette() {
+        this.hideMonsterPage();
         this.curPage = 0;
         this.recetteButton.setTint(0xffffff, 0xffffff, 0xff0000, 0xffffff);
         this.bestiaireButton.clearTint();
         this.dansRecette = true;
         this.curPageText.setText("1");
+        this.chargeRecipePage();
 
     }
 
     switch_bestaire() {
+        this.hideRecipePage();
         this.curPage = 0;
         this.bestiaireButton.setTint(0xffffff, 0xffffff, 0xff0000, 0xffffff);
         this.recetteButton.clearTint();
         this.dansRecette = false;
         this.curPageText.setText("1");
+        this.chargeMonsterPage();
     }
 
     nextPage(left = false) {
@@ -118,12 +153,31 @@ export class Receuil extends Phaser.Scene {
 
     //Charge la page courante de monstre
     chargeMonsterPage() {
-
+        this.hideMonsterPage();
     }
 
     //Charge la page courante de recette
     chargeRecipePage() {
-        
+    
+        this.hideRecipePage();
+        var size = nb_col_display*nb_row_display;
+        var lim = Math.min(this.curPage*size+size, this.total_recipe);
+        for (var i = this.curPage*size; i < lim ;i++) {
+            this.recipesPres[i-(size*this.curPage)].change_recipe(this.recipes[i]);
+
+        }
+
+    }
+
+
+    hideMonsterPage() {
+        //TODO
+    }
+
+    hideRecipePage() {
+        for (var i = 0; i < nb_col_display*nb_row_display; i++) {
+            this.recipesPres[i].switch_visibility(false);
+        }
     }
 
     update() {
