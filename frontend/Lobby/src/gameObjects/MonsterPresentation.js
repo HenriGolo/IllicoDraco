dbfa0cf7988fea4510ad9bb2e80b702b51b5649c
jme7710 <jme7710@pippin.enseeiht.fr>
2026-05-06 13:50:01 +0200

    monter presentation

--- frontend/Lobby/src/gameObjects/MonsterPresentation.js
+++ frontend/Lobby/src/gameObjects/MonsterPresentation.js
@@ -1,17 +1,67 @@
 //Classe pour afficher un monstre dans le bestiaire
 
+const zoom = 3;
 const width = 300;
 const height = 294;
 
 export default class MonsterPresentation {
 
-  constructor(scene, graphics, x = 0, y = 92, monster = null) {
-        
-        graphics.fillStyle(0xFF0000, 1);
-        graphics.fillRect(x, y, width, height);
+   constructor(scene, x = 0, y = 92){
+        this.scene = scene;
+        this.x = x;
+        this.y = y;
 
-        console.log("ok");
+        this.graphics = scene.add.graphics();
+        this.graphics.lineStyle(4, 0xc1c1c1, 1.0);
+        this.graphics.fillStyle(0x6b4b34, 1);
 
+        this.graphics.fillRect(x, y, width, height);
+        this.graphics.strokeRect(x, y, width, height);
+
+        //this.graphics.lineBetween(x, y + 110, x + width, y + 110);
+
+        //this.graphics.lineBetween(x, y + 178, x + width, y + 178);
+
+        this.monster_name = scene.add.text(x, y+10, "", { fontSize: '32px', fill: '#FFF' })
+          .setFixedSize(width, height)
+          .setAlign('center');
+
+        this.monster_img = null;
+
+        this.monster_info = scene.add.text(x + 50, y + 120,  "" ,{ fontSize: '16px', fill: '#FFF' });
+       
+       this.graphics.setVisible(false);
+
+  }
+
+
+  change_monster(monster) {
+    
+    if (this.monster_img != null) {
+      this.monster_img.destroy();
+
+    }
+
+    this.monster_name.setText(monster.getName());    
+    this.monster_info.setText("- vie : " + monster.getVie() +
+      "\n- attaque : " + monster.getAttaque() + 
+      "\n- defense : " + monster.getDefense() +
+      "\n- vitesse : " + monster.getVitesse());
+
+    this.monster_img =  this.scene.add.sprite( this.x+(width/2),  this.y+84, monster.getImage())
+      .setScale(zoom, zoom);
+
+    this.switch_visibility(true);
+  }
+
+  switch_visibility(b) {
+    this.graphics.setVisible(b);
+    this.monster_name.setVisible(b);
+    this.monster_info.setVisible(b);
+
+    if (this.monster_img != null) {
+      this.monster_img.setVisible(b);
+    }
   }
 
 }
