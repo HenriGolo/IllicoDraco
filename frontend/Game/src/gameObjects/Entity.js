d403d12dce2f250aa176a695e42ba567cbeb07d9
JulMig <julie.mignotte8@gmail.com>
2026-05-18 19:34:23 +0200

    barre de vie finit

--- frontend/Game/src/gameObjects/Entity.js
+++ frontend/Game/src/gameObjects/Entity.js
@@ -25,7 +25,7 @@ export default class Entity {
         this.lifeBar_back.setX(x)
         this.lifeBar_back.setY(y-11)
 
-        this.lifeBar.setX(x)
+        this.lifeBar.setX(x-(8-(this.lifeBar.width/2)))
         this.lifeBar.setY(y-11)
     }
 
@@ -39,6 +39,7 @@ export default class Entity {
             this.is_dead = true
         }
         this.lifeBar.setSize((this.pv*16)/this.max_pv, 4)
+        this.lifeBar.setX(this.lifeBar.x-(8-(this.lifeBar.width/2)))
     }
 
     getDef() {
