430c0d2c350f7559297b9e16045b730a4a6dec75
celinda42 <112560667+celinda42@users.noreply.github.com>
2026-05-19 22:41:02 +0200

    ajout du load des plats finis

--- frontend/Game/src/scenes/Preloader.js
+++ frontend/Game/src/scenes/Preloader.js
@@ -125,6 +125,12 @@ export class Preloader extends Phaser.Scene {
       this.load.image(ingredients[i], 'assets/ingredients/' + ingredients[i] + '.png')
     }
 
+    let plats_finis = ['atchoum', 'boeilgur', 'brioche', 'carotte_genie', 'fromage', 'jackoNavet', 'tarte', 'pain', 'ragout', 'slimeFresh', 'soupeLegumes', 'tourLegumes']
+
+    for (let i = 0; i < plats_finis.length; i++) {
+      this.load.image(plats_finis[i], 'assets/ingredients/' + plats_finis[i] + '.png')
+    }
+
   }
 
 
