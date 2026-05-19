053b0645d6939b47ef6a45aa97ed8dc13f98264f
Henri Golo <henri.golo.313f@gmail.com>
2026-05-19 13:51:37 +0200

    prise en charge d'un objet ramassé par les joueurs

---
+++ backend/src/main/java/illicodraco/project/game/ObjetRamasse.java
@@ -0,0 +1,25 @@
+package illicodraco.project.game;
+
+public class ObjetRamasse {
+
+  /// Clé (cf frontend)
+  private String key;
+  /// Indice de l'objet ramassé
+  private int indice;
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public int getIndice() {
+    return indice;
+  }
+
+  public void setIndice(int indice) {
+    this.indice = indice;
+  }
+}
